9665c4fb75b511af24aacf78acd7437366d4469a
nazi-tarun <tarun@example.com>
2025-12-28 19:57:26 +0530

    chore: remove load test files

--- artillery-processor.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  logCacheStatus: function(requestParams, response, context, ee, next) {
-    if (response.body) {
-      const body = JSON.parse(response.body)
-      console.log(`Cache status: ${body.cached ? 'HIT' : 'MISS'}`)
-    }
-    return next()
-  }
-}
